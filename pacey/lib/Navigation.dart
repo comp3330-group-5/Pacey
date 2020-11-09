d50cd8f94fc3611f4374515f1c73932e844a04f8
darrenleung1208hk <leungyatlong1208@gmail.com>
2020-11-09 23:10:40 +0800

    separated classes into distinct files

---
+++ pacey/lib/Navigation.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class Navigation extends StatefulWidget {
+  Navigation({Key key}) : super(key: key);
+
+  @override
+  _NavigationState createState() => _NavigationState();
+}
+
+/// This is the private State class that goes with Navigation.
+class _NavigationState extends State<Navigation> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    // RunPage(),
+    // RoutesPage(),
+    // StatisticsPage(),
+    // ProfilePage(),
+    Text(
+      'RunPage',
+      style: optionStyle,
+    ),
+    Text(
+      'RoutesPage',
+      style: optionStyle,
+    ),
+    Text(
+      'StatisticsPage',
+      style: optionStyle,
+    ),
+    Text(
+      'ProfilePage',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.sports),
+            label: 'Run',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Routes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.analytics),
+            label: 'Statistics',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
--- pacey/lib/main.dart
+++ pacey/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './Navigation.dart';
 
 void main() => runApp(Pacey());
 
@@ -6,66 +7,7 @@ class Pacey extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pacey',
-      home: BottomNavigation(),
-    );
-  }
-}
-
-class BottomNavigation extends StatefulWidget {
-  BottomNavigation({Key key}) : super(key: key);
-
-  @override
-  _BottomNavigationState createState() => _BottomNavigationState();
-}
-
-/// This is the private State class that goes with BottomNavigation.
-class _BottomNavigationState extends State<BottomNavigation> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    RunPage(),
-    RoutePage(),
-    StatisticsPage(),
-    ProfilePage(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.sports),
-            label: 'Run',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map),
-            label: 'Routes',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.analytics),
-            label: 'Statistics',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_box),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
+      home: Navigation(),
     );
   }
 }
