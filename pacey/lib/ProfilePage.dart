632247c53794f40f852d5d752fddcdab295edae1
darrenleung1208hk <leungyatlong1208@gmail.com>
2020-11-30 04:30:13 +0800

    profile page age row

--- pacey/lib/ProfilePage.dart
+++ pacey/lib/ProfilePage.dart
@@ -1,12 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key key}) : super(key: key);
+class ProfilePage extends StatefulWidget {
+  ProfilePage({Key key}) : super(key: key);
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  DateTime selectedDate = DateTime.now();
+
+  _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1940),
+      lastDate: DateTime(2021),
+      helpText: 'Select your birthday',
+    );
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        // child: child,
-        );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Profile',
+          style: GoogleFonts.roboto(
+            textStyle: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 28,
+              color: Colors.black87,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              'Age:',
+              style: GoogleFonts.roboto(
+                textStyle: TextStyle(fontSize: 22, color: Colors.black87),
+              ),
+            ),
+            SizedBox(width: 20),
+            Text(
+              "${selectedDate.toLocal()}".split(' ')[0],
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(width: 20),
+            RaisedButton(
+              onPressed: () => _selectDate(context),
+              child: Text(
+                'Select date',
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+              color: Colors.greenAccent,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
