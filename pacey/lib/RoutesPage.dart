2ab4e2de0eadaa149d59d13c0fb1e98cb27e853f
lamtingfaijoe <75008525+lamtingfaijoe@users.noreply.github.com>
2020-12-07 03:11:03 +0800

    Update RoutesPage.dart

--- pacey/lib/RoutesPage.dart
+++ pacey/lib/RoutesPage.dart
@@ -1,11 +1,23 @@
 import 'dart:async';
+import 'dart:collection';
+import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-class RoutesPage extends StatelessWidget {
+class RoutesPage extends StatefulWidget {
+  RoutesPage({Key key}) : super(key: key);
+
+  @override
+  _RoutesState createState() => _RoutesState();
+}
+
+class _RoutesState extends State<RoutesPage> {
   Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
+  Location _locationTracker = Location();
+  List<Marker> myMarker = [];
+  LatLng ini_postion = LatLng(37.42796133580664, -122.085749655962);
+  static final CameraPosition _initialPosition = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
@@ -20,18 +32,62 @@ class RoutesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
+        mapType: MapType.normal,
+        initialCameraPosition: _initialPosition,
+        markers: Set.from(myMarker),
+        onTap: _handleTap,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
+
+          setState(() {
+            myMarker.add(
+              Marker(
+                markerId: MarkerId("0"),
+                position: ini_postion,
+                infoWindow: InfoWindow(
+                    title: "Starting Point", snippet: "An good place"),
+              ),
+            );
+          });
         },
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.location_searching),
+        onPressed: () {},
       ),
     );
+  }
+
+  _handleTap(LatLng tappedPoint) {
+    print(tappedPoint);
+    setState(() {
+      myMarker = []; //this make the only one marker on the map
+      myMarker.add(
+        Marker(
+          markerId: MarkerId(tappedPoint.toString()),
+          position: tappedPoint,
+          infoWindow: InfoWindow(
+              title: "Ending Point", snippet: "Here is your end point"),
+        ),
+      );
+      myMarker.add(Marker(
+        markerId: MarkerId("0"),
+        position: ini_postion,
+        infoWindow:
+            InfoWindow(title: "Starting Point", snippet: "An good place"),
+      ));
+    });
+  }
+
+  void updateMarker(LocationData newLocalData /*,marker imagedata)*/) {
+    LatLng latLng = LatLng(newLocalData.latitude, newLocalData.longitude);
+    //this.setstate reset the the marker location
+  }
+
+  void getCurrentLocation() async {
+    //get the marker
+    //var location = await _locationTracker.getlocation();
+    //updateMarker
   }
 
   Future<void> _goToTheLake() async {
